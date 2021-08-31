3e4bf8eec7d83915eb104e786cdbe6d3e1740486
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-08-31 12:32:08 +0100

    Create mobile menu branch and add links when the hamburger icon is clicked

---
+++ script.js
@@ -0,0 +1,44 @@
+const mobileMenuDiv = document.querySelector('#mobile-menu');
+mobileMenuDiv.className = 'mobile-menu-hidden';
+// Create closing icon and attach to div
+let closingIcondiv = document.createElement('div');
+closingIcondiv.className = "menu-item";
+closingIcondiv.className = "closing-icon-item";
+let anchorTag = document.createElement('a');
+anchorTag.setAttribute('href', '#');
+let closingIcon = document.createElement('IMG');
+closingIcon.setAttribute('src', 'Images/closing-icon.png');
+closingIcon.setAttribute('alt', 'Icon image');
+closingIcon.id = 'close-button';
+anchorTag.appendChild(closingIcon);
+closingIcondiv.appendChild(anchorTag);
+mobileMenuDiv.appendChild(closingIcondiv);
+// Create portfolio link item and attach to the mobile menu div
+let portfolioLinkDiv = document.createElement('div');
+portfolioLinkDiv.className = "menu-item";
+anchorTag = document.createElement('a');
+anchorTag.className = 'nav-menu-link';
+anchorTag.textContent = 'Portfolio';
+anchorTag.setAttribute('href', '#cards-section');
+portfolioLinkDiv.appendChild(anchorTag);
+mobileMenuDiv.appendChild(portfolioLinkDiv);
+// Create about link section and attach to the mobile menu div
+let aboutDiv = document.createElement('div');
+aboutDiv.className = "menu-item";
+anchorTag = document.createElement('a');
+anchorTag.className = 'nav-menu-link';
+anchorTag.textContent = 'About';
+anchorTag.setAttribute('href', '#about');
+aboutDiv.appendChild(anchorTag);
+mobileMenuDiv.appendChild(aboutDiv);
+// Create contact link section and attach to the mobile menu div
+let contactDiv = document.createElement('div');
+contactDiv.className = "menu-item";
+anchorTag = document.createElement('a');
+anchorTag.className = 'nav-menu-link';
+anchorTag.textContent = 'Contact';
+anchorTag.setAttribute('href', '#contacts');
+contactDiv.appendChild(anchorTag);
+mobileMenuDiv.appendChild(contactDiv);
+let menuButton = document.querySelector('#menu-button');
+
