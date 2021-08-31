e2c5ea193ce8b3cb666b70214bf683346dcb3baf
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-08-31 13:39:37 +0100

    Fix linters errors in script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const mobileMenuDiv = document.querySelector('#mobile-menu');
+
 mobileMenuDiv.className = 'mobile-menu-hidden';
 // Create closing icon and attach to div
-let closingIcondiv = document.createElement('div');
-closingIcondiv.className = "menu-item";
-closingIcondiv.className = "closing-icon-item";
+const closingIcondiv = document.createElement('div');
+closingIcondiv.className = 'menu-item';
+closingIcondiv.className = 'closing-icon-item';
 let anchorTag = document.createElement('a');
 anchorTag.setAttribute('href', '#');
-let closingIcon = document.createElement('IMG');
+const closingIcon = document.createElement('IMG');
 closingIcon.setAttribute('src', 'Images/closing-icon.png');
 closingIcon.setAttribute('alt', 'Icon image');
 closingIcon.id = 'close-button';
@@ -14,8 +15,8 @@ anchorTag.appendChild(closingIcon);
 closingIcondiv.appendChild(anchorTag);
 mobileMenuDiv.appendChild(closingIcondiv);
 // Create portfolio link item and attach to the mobile menu div
-let portfolioLinkDiv = document.createElement('div');
-portfolioLinkDiv.className = "menu-item";
+const portfolioLinkDiv = document.createElement('div');
+portfolioLinkDiv.className = 'menu-item';
 anchorTag = document.createElement('a');
 anchorTag.className = 'nav-menu-link';
 anchorTag.textContent = 'Portfolio';
@@ -23,8 +24,8 @@ anchorTag.setAttribute('href', '#cards-section');
 portfolioLinkDiv.appendChild(anchorTag);
 mobileMenuDiv.appendChild(portfolioLinkDiv);
 // Create about link section and attach to the mobile menu div
-let aboutDiv = document.createElement('div');
-aboutDiv.className = "menu-item";
+const aboutDiv = document.createElement('div');
+aboutDiv.className = 'menu-item';
 anchorTag = document.createElement('a');
 anchorTag.className = 'nav-menu-link';
 anchorTag.textContent = 'About';
@@ -32,30 +33,32 @@ anchorTag.setAttribute('href', '#about');
 aboutDiv.appendChild(anchorTag);
 mobileMenuDiv.appendChild(aboutDiv);
 // Create contact link section and attach to the mobile menu div
-let contactDiv = document.createElement('div');
-contactDiv.className = "menu-item";
+const contactDiv = document.createElement('div');
+contactDiv.className = 'menu-item';
 anchorTag = document.createElement('a');
 anchorTag.className = 'nav-menu-link';
 anchorTag.textContent = 'Contact';
 anchorTag.setAttribute('href', '#contacts');
 contactDiv.appendChild(anchorTag);
 mobileMenuDiv.appendChild(contactDiv);
-let menuButton = document.querySelector('#menu-button');
+const menuButton = document.querySelector('#menu-button');
 
-function display () {
-    const targetedDiv = document.querySelector('.mobile-menu-hidden');
-    targetedDiv.classList.add('show');
-    menuButton.style.display = 'none';
+function display() {
+  const targetedDiv = document.querySelector('.mobile-menu-hidden');
+  targetedDiv.classList.add('show');
+  menuButton.style.display = 'none';
 }
-menuButton.addEventListener('click',display);
+
+menuButton.addEventListener('click', display);
 const closeButton = document.querySelector('#close-button');
-function closePop () {
-    const targetedDiv2 = document.querySelector('.mobile-menu-hidden');
-    targetedDiv2.classList.remove('show');
-    menuButton.style.display = 'block';
+function closePop() {
+  const targetedDiv2 = document.querySelector('.mobile-menu-hidden');
+  targetedDiv2.classList.remove('show');
+  menuButton.style.display = 'block';
 }
-closeButton.addEventListener('click',closePop);
+
+closeButton.addEventListener('click', closePop);
 const anchorTagLinks = document.querySelectorAll('.nav-menu-link');
 anchorTagLinks.forEach((link) => {
-    link.addEventListener('click', closePop);
-})
+  link.addEventListener('click', closePop);
+});
