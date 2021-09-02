3b54b4068bbd4d152e83d4dc3a981f7ca4854201
Whoistolu <digittolu25@gmail.com>
2021-09-02 19:00:01 +0100

    Add event event listener to the form in the form js file

--- form.js
+++ form.js
@@ -2,3 +2,11 @@ const emailAddress = document.getElementById('user_email');
 // const filteredEmail = emailAddress.replace(/\W/gi, '');
 const templateForm = document.getElementById('form');
 const validatorMessage = document.getElementById('validator-message');
+templateForm.addEventListener('submit', (e) => {
+    if (emailAddress.value !== emailAddress.value.toLowerCase()) {
+      e.preventDefault();
+      validatorMessage.textContent = 'Kindly input email in lowercase';
+    } else {
+      validatorMessage.textContent = '';
+    }
+  });
