d02d956a7ebf1a1ff3afc9ea4eb7b2102ba8545f
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-02 19:05:49 +0100

    Commit changes made by coding partners to the form controls

--- form.js
+++ form.js
@@ -3,10 +3,10 @@ const emailAddress = document.getElementById('user_email');
 const templateForm = document.getElementById('form');
 const validatorMessage = document.getElementById('validator-message');
 templateForm.addEventListener('submit', (e) => {
-    if (emailAddress.value !== emailAddress.value.toLowerCase()) {
-      e.preventDefault();
-      validatorMessage.textContent = 'Kindly input email in lowercase';
-    } else {
-      validatorMessage.textContent = '';
-    }
-  });
+  if (emailAddress.value !== emailAddress.value.toLowerCase()) {
+    e.preventDefault();
+    validatorMessage.textContent = 'Kindly input email in lowercase';
+  } else {
+    validatorMessage.textContent = '';
+  }
+});
