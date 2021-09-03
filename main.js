8e3550cdc64907ac3023f11c08bc21cdcaa90f61
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-03 14:24:44 +0100

    Implement all changes required by the code reviewer in the main.js file

--- main.js
+++ main.js
@@ -1,125 +1,83 @@
-// Create object to store data needed for the this branch feature
+// // Create object to store data needed for the this branch feature
 const projects = [
   {
-    id: 1,
     name: 'Multi-Post Stories',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. 
-    Lorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.`,
-    featuredImage: {
-      image1: './Images/Closing-Icon.png',
-      image2: './Images/Snapshoot-Image.png',
-      image3: './Images/Snapshoot-Image2.png',
-      image4: './Images/Live-Icon.png',
-      image5: './Images/Github-Logo.png',
-    },
-    technologies: ['HTML', 'Ruby on rails', 'CSS', 'Github'],
-    links: [' https://gambit142.github.io/Template-Portfolio/', 'https://github.com/Gambit142/Template-Portfolio'],
+    description: `A daily selection of privately personalized reads; no accounts
+          or sign-ups required. has been the industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a
+          standard dummy text.`,
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
   },
   {
-    id: 2,
     name: 'Multi-Post Stories',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. 
-    Lorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.`,
-    featuredImage: {
-      image1: './Images/Closing-Icon.png',
-      image2: './Images/Snapshoot-Image.png',
-      image3: './Images/Snapshoot-Image2.png',
-      image4: './Images/Live-Icon.png',
-      image5: './Images/Github-Logo.png',
-    },
-    technologies: ['HTML', 'Ruby on rails', 'CSS', 'Github'],
-    links: [' https://gambit142.github.io/Template-Portfolio/', 'https://github.com/Gambit142/Template-Portfolio'],
+    description: `A daily selection of privately personalized reads; no accounts
+          or sign-ups required. has been the industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a
+          standard dummy text.`,
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
   },
   {
-    id: 3,
     name: 'Multi-Post Stories',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. 
-    Lorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.`,
-    featuredImage: {
-      image1: './Images/Closing-Icon.png',
-      image2: './Images/Snapshoot-Image.png',
-      image3: './Images/Snapshoot-Image2.png',
-      image4: './Images/Live-Icon.png',
-      image5: './Images/Github-Logo.png',
-    },
-    technologies: ['HTML', 'Ruby on rails', 'CSS', 'Github'],
-    links: [' https://gambit142.github.io/Template-Portfolio/', 'https://github.com/Gambit142/Template-Portfolio'],
+    description: `A daily selection of privately personalized reads; no accounts
+          or sign-ups required. has been the industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a
+          standard dummy text.`,
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
   },
   {
-    id: 4,
     name: 'Multi-Post Stories',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. 
-    Lorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.`,
-    featuredImage: {
-      image1: './Images/Closing-Icon.png',
-      image2: './Images/Snapshoot-Image.png',
-      image3: './Images/Snapshoot-Image2.png',
-      image4: './Images/Live-Icon.png',
-      image5: './Images/Github-Logo.png',
-    },
-    technologies: ['HTML', 'Ruby on rails', 'CSS', 'Github'],
-    links: [' https://gambit142.github.io/Template-Portfolio/', 'https://github.com/Gambit142/Template-Portfolio'],
+    description: `A daily selection of privately personalized reads; no accounts
+          or sign-ups required. has been the industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a
+          standard dummy text.`,
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
   },
 ];
-// Create the modal section using string literals
+// // Create the cards section dynamically using string literals
 function createCard1(project) {
-  const cardString = `<div id='modal-${project.id}' class="pop-up-container display-flex" id="modal-content">
-  <div class="title-container display-flex style">
-    <h2>${project.name}</h2>
-    <img class="closing-button" src= ${project.featuredImage.image1} alt="Closing Icon">
-  </div>
-  <div class="image-placeholder display-flex">
-    <img class="snapshot1" src= ${project.featuredImage.image2} alt="Snapshot of a card design">
-    <img class="snapshot2" src= ${project.featuredImage.image3} alt="Snapshot of a card design">
-  </div>
-  <div class="card-description display-flex">
-    <p class="card-text style">${project.description}</p>
-  </div>
-  <div class="card-badges display-flex">
-    <h4 class="badge html">${project.technologies[0]}</h4>
-    <h4 class="badge">${project.technologies[1]}</h4>
-    <h4 class="badge">${project.technologies[2]}</h4>
-    <h4 class="badge github">${project.technologies[3]}</h4>
-  </div>
-  <div class="links-div display-flex">
-    <a href= ${project.links[0]} class="live-link">
-      See Live
-      <img class="live-icon" src=${project.featuredImage.image4} alt="Live Icon">
-    </a>
-    <a href= ${project.links[1]} class="source-link">
-      See Source
-      <img class="github-icon" src=${project.featuredImage.image5} alt="Github Icon">
-    </a>
-  </div>
-</div>`;
+  const cardString = `<section class="work-container">
+  <article class="cards-container display-flex">
+    <div class="placeholder"></div>
+    <div class="details-div display-flex">
+      <div class="cards-header display-flex">
+        <h3 class="style">${project.name}</h3>
+      </div>
+      <div class="card-info">
+        <p class="style">
+        ${project.description}
+        </p>
+      </div>
+      <ul class="languages-div">
+        <li class="lang style">${project.technologies[0]}</li>
+        <li class="lang style">${project.technologies[1]}</li>
+        <li class="lang style">${project.technologies[2]}</li>
+        <li class="ruby lang style">${project.technologies[3]}</li>
+      </ul>
+      <div class="btn-div">
+        <button type="button" class="see-project">
+          See Project
+        </button>
+      </div>
+    </div>
+  </article>
+</section>`;
   return cardString;
 }
-// Create functions that opens and close the popup window when see-project button is clicked
-const workArea = document.getElementById('modal-container');
+const projectContainer = document.querySelector('main');
+
 projects.forEach((card) => {
-  workArea.innerHTML += createCard1(card);
+  projectContainer.innerHTML += createCard1(card);
 });
-// function displayModal() {
-//   workArea.style.display = 'block';
-// }
+// Create functions that opens and close the popup window when see-project button is clicked
+const workArea = document.querySelector('.container');
 const projectButtons = document.querySelectorAll('.see-project');
 projectButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    const currentModal = document.querySelector(button.dataset.target);
-    currentModal.style.display = 'block';
+    workArea.style.display = 'block';
   });
 });
-const closingButtons = document.querySelectorAll('.closing-button');
-// function closeModal() {
-//   workArea.style.display = 'none';
-// }
-closingButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const currentModal = button.closest('pop-up-container');
-    currentModal.style.display = 'none';
-  });
+
+const closingButtons = document.querySelector('.closing-button');
+closingButtons.addEventListener('click', () => {
+  workArea.style.display = 'none';
 });
