5704dec182361df833c184ea664719e74c62fe13
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-31 20:15:56 +0100

    Give animations ease-in and ease-out to modal window

--- main.js
+++ main.js
@@ -111,34 +111,36 @@ function createCard1(project) {
 
 function createModal(project) {
   const modalInner = `
-<div class="pop-up-container display-flex" id="modal${project.number}">
-<img class="closing-button" src="./Images/Closing-Icon.png" alt="Closing Icon">
-<div class="title-container display-flex style">
-  <h2>${project.name}</h2>
-</div>
-<div class="image-placeholder display-flex">
-  <img class="snapshot1" src="${project.image.imageUrl}" alt="${project.image.imageAltText}">
-  <img class="snapshot2" src= "${project.image.imageUrl2}" alt="${project.image.imageAltText}">
-</div>
-<div class="card-description display-flex">
-  <p class="card-text style">${project.description2}</p>
-</div>
-<div class="card-badges display-flex">
-  <h4 class="badge html">${project.technologies[0]}</h4>
-  <h4 class="badge">${project.technologies[1]}</h4>
-  <h4 class="badge">${project.technologies[2]}</h4>
-  <h4 class="badge github">${project.technologies[3]}</h4>
-</div>
-<div class="links-div display-flex">
-  <a href= ${project.links[0]} class="live-link">
-    See Live
-    <img class="live-icon" src="./Images/Live-Icon.png" alt="Live Icon">
-  </a>
-  <a href= ${project.links[1]} class="source-link">See Source
-    <img class="github-icon" src="./Images/Github-Logo.png" alt="Github Icon">
-  </a>
-</div>
-</div>`;
+    <div class="pop-up-container display-flex" id="modal${project.number}">
+    <img class="closing-button" src="./Images/Closing-Icon.png" alt="Closing Icon">
+      <div class="modal-window modal${project.number}">
+        <div class="title-container display-flex style">
+          <h2>${project.name}</h2>
+        </div>
+        <div class="image-placeholder display-flex">
+          <img class="snapshot1" src="${project.image.imageUrl}" alt="${project.image.imageAltText}">
+          <img class="snapshot2" src= "${project.image.imageUrl2}" alt="${project.image.imageAltText}">
+        </div>
+        <div class="card-description display-flex">
+          <p class="card-text style">${project.description2}</p>
+        </div>
+        <div class="card-badges display-flex">
+          <h4 class="badge html">${project.technologies[0]}</h4>
+          <h4 class="badge">${project.technologies[1]}</h4>
+          <h4 class="badge">${project.technologies[2]}</h4>
+          <h4 class="badge github">${project.technologies[3]}</h4>
+        </div>
+        <div class="links-div display-flex">
+          <a href= ${project.links[0]} class="live-link">
+            See Live
+            <img class="live-icon" src="./Images/Live-Icon.png" alt="Live Icon">
+          </a>
+          <a href= ${project.links[1]} class="source-link">See Source
+            <img class="github-icon" src="./Images/Github-Logo.png" alt="Github Icon">
+          </a>
+        </div>
+      </div>
+    </div>`;
   return modalInner;
 }
 
@@ -160,7 +162,13 @@ const projectButtons = document.querySelectorAll('.see-project');
 projectButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const currentModal = document.getElementById(button.dataset.target);
+    const modalWindows = document.querySelectorAll('.modal-window');
     currentModal.classList.add('active');
+    modalWindows.forEach((window) => {
+      window.classList.remove('remove-zoom');
+      window.classList.add('add-zoom');
+    });
+    currentModal.style.display = 'flex';
   });
 });
 
@@ -169,6 +177,14 @@ const closingButtons = document.querySelectorAll('.closing-button');
 closingButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const currentModal = document.querySelector('.active');
-    currentModal.classList.remove('active');
+    const modalWindows = document.querySelectorAll('.modal-window');
+    modalWindows.forEach((window) => {
+      window.classList.remove('add-zoom');
+      window.classList.add('remove-zoom');
+    });
+    setTimeout(() => {
+      currentModal.classList.remove('active');
+      currentModal.style.display = 'none';
+    }, 1000);
   });
 });
